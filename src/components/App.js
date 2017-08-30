597711579f844a8c9bd8830420b7f9ea8eddf2fd
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-08-30 09:26:21 -0400

    cleaning up

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,7 @@ import GetImageForm from './GetImageForm'
 export default class App extends Component {
   render() {
     return (
-      <div>
       <GetImageForm />
-      </div>
     );
   }
 }
--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -27,8 +27,8 @@ export default class GetImageForm extends Component{
     let camera = this.state.camera
     let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${API_KEY}`;
     fetch(imageUrl)
-    .then((res)=> res.json())
-    .then((json)=>{
+    .then(res => res.json())
+    .then(json =>{
       this.setState({
         images:json.photos
       })
@@ -56,18 +56,18 @@ export default class GetImageForm extends Component{
     return(
       <div>
       <label htmlFor="rover">Rover</label>
-      <select onChange={this.handleRover} id="rover" value={this.state.value}>
+      <select onChange={this.handleRover} id="rover" value={this.state.rover}>
         <option value="Curiosity">Curiosity</option>
         <option value="Opportunity">Opportunity</option>
         <option value="Spirit">Spirt</option>
       </select>
-      <label htmlFor="camera">Camera Type</label><select onChange={this.handleCamera} id="rover" value={this.state.value}>
+      <label htmlFor="camera">Camera Type</label><select onChange={this.handleCamera} id="camera" value={this.state.camera}>
         <option value="fhaz">FHAZ (Front Hazard)</option>
         <option value="rhaz">RHAZ (Rear Hazard)</option>
         <option value="navcam">NAVCAM (Navigation Cam)</option>
       </select>
       <label htmlFor="sol">Martian Sol: 1000-2000</label>
-      <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
+      <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.sol}/>
       <GetImageButton handleClick={this.fetchRoverImage} />
       <ImageDisplay images={this.state.images} />
       </div>
